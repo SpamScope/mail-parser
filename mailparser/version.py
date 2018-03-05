05025f0f3b8f97d8b5a0fd15bbaf5e232bb264de
Fedele Mantuano <mantuano.fedele@gmail.com>
2018-03-05 23:45:39 +0100

    Bugfix #26

--- mailparser/version.py
+++ mailparser/version.py
@@ -17,7 +17,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-__version__ = "3.2.6"
+__version__ = "3.2.7"
 
 if __name__ == "__main__":
     print(__version__)
