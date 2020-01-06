81bb04692ed96c05fc166ddc52c3472fcfa28a4d
Fedele Mantuano <mantuano.fedele@gmail.com>
2020-01-06 21:16:11 +0100

    Bugfix issue #61

--- mailparser/utils.py
+++ mailparser/utils.py
@@ -344,7 +344,7 @@ def convert_mail_date(date):
     t = email.utils.mktime_tz(d)
     log.debug("Date parsed in timestamp: {!r}".format(t))
     date_utc = datetime.datetime.utcfromtimestamp(t)
-    timezone = d[9] / 3600 if d[9] else 0
+    timezone = d[9] / 3600.0 if d[9] else 0
     timezone = "{:+.1f}".format(timezone)
     log.debug("Calculated timezone: {!r}".format(timezone))
     return date_utc, timezone
