d6809f5c1a508619db1390fe0936cde67b5ec7d8
Fedele Mantuano <mantuano.fedele@gmail.com>
2020-01-12 14:51:56 +0100

    Removed README.rst. Added attachments guide issue #59

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup
 
 current = os.path.realpath(os.path.dirname(__file__))
 
-with io.open(os.path.join(current, 'README.rst'), encoding="utf-8") as f:
+with io.open(os.path.join(current, 'README.md'), encoding="utf-8") as f:
     long_description = f.read()
 
 with open(os.path.join(current, 'requirements.txt')) as f:
