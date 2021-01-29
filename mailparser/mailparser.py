0a8b2a4f97633e221f48636ccb4a1b6e7876ad53
Nitish Kansal <tanu.kansal@gmail.com>
2021-01-29 20:52:45 +0530

    If CTE is not available then also we should decode the payload which python encoded to keep encodings intact

--- mailparser/mailparser.py
+++ mailparser/mailparser.py
@@ -423,7 +423,7 @@ class MailParser(object):
                     cte = p.get('Content-Transfer-Encoding')
                     if cte:
                         cte = cte.lower()
-                    if cte in ['7bit', '8bit']:
+                    if not cte or cte in ['7bit', '8bit']:
                         payload = payload.decode('raw-unicode-escape')
                     else:
                         payload = ported_string(payload, encoding=charset)
