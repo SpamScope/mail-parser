b58f9bfc7065fe8912f0e0b73d8ed1aafec6c43e
Fedele Mantuano <fmantuano@blackberry.com>
2024-03-20 00:51:03 +0100

    Updated actions

--- src/mailparser/utils.py
+++ src/mailparser/utils.py
@@ -587,4 +587,4 @@ def random_string(string_length=10):
         str -- Random string
     """
     letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(string_length))
+    return ''.join(random.choice(letters) for _ in range(string_length))
