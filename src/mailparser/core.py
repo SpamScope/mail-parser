2b79bb6129904eed7979f363fde0e76222fd410d
Sean Whalen <seanthegeek@users.noreply.github.com>
2025-03-24 08:32:09 -0400

    Fix missing object attributes

--- src/mailparser/core.py
+++ src/mailparser/core.py
@@ -601,7 +601,7 @@ class MailParser(object):
                                       parsed_address[-1].strip(">"))
                     return parsed_address
 
-        elif name_header in ADDRESSES_HEADERS:
+        if name_header in ADDRESSES_HEADERS:
             h = decode_header_part(self.message.get(name_header, six.text_type()))
             if h == "":
                 return []
